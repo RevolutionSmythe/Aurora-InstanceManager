2357ffad9f84f5fd1a40c3730c123a1e5f9d2f13
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-05-26 20:02:49 -0500

    Add the first parts of the InstanceManager for Aurora-Sim.

---
+++ InstanceManager/InstanceManager/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace InstanceManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.button1 = new System.Windows.Forms.Button ();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog ();
+            this.button2 = new System.Windows.Forms.Button ();
+            this.ConfigFilePath = new System.Windows.Forms.TextBox ();
+            this.label1 = new System.Windows.Forms.Label ();
+            this.label2 = new System.Windows.Forms.Label ();
+            this.AuroraPath = new System.Windows.Forms.TextBox ();
+            this.button3 = new System.Windows.Forms.Button ();
+            this.panel1 = new System.Windows.Forms.Panel ();
+            this.panel2 = new System.Windows.Forms.Panel ();
+            this.button4 = new System.Windows.Forms.Button ();
+            this.initoGenerateName = new System.Windows.Forms.TextBox ();
+            this.listBox1 = new System.Windows.Forms.ListBox ();
+            this.label3 = new System.Windows.Forms.Label ();
+            this.button5 = new System.Windows.Forms.Button ();
+            this.groupBox1 = new System.Windows.Forms.GroupBox ();
+            this.button6 = new System.Windows.Forms.Button ();
+            this.label4 = new System.Windows.Forms.Label ();
+            this.button7 = new System.Windows.Forms.Button ();
+            this.RegionsDir = new System.Windows.Forms.TextBox ();
+            this.button8 = new System.Windows.Forms.Button ();
+            this.panel3 = new System.Windows.Forms.Panel ();
+            this.panel1.SuspendLayout ();
+            this.panel2.SuspendLayout ();
+            this.groupBox1.SuspendLayout ();
+            this.panel3.SuspendLayout ();
+            this.SuspendLayout ();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point (227, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size (75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Migrate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler (this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point (146, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size (75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler (this.button2_Click);
+            // 
+            // ConfigFilePath
+            // 
+            this.ConfigFilePath.Location = new System.Drawing.Point (2, 19);
+            this.ConfigFilePath.Name = "ConfigFilePath";
+            this.ConfigFilePath.ReadOnly = true;
+            this.ConfigFilePath.Size = new System.Drawing.Size (138, 20);
+            this.ConfigFilePath.TabIndex = 2;
+            this.ConfigFilePath.Text = "C:\\\\AuroraConfig";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point (2, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size (138, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "New Configuration File Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point (2, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size (83, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Aurora.exe Path";
+            // 
+            // AuroraPath
+            // 
+            this.AuroraPath.Location = new System.Drawing.Point (2, 68);
+            this.AuroraPath.Name = "AuroraPath";
+            this.AuroraPath.ReadOnly = true;
+            this.AuroraPath.Size = new System.Drawing.Size (138, 20);
+            this.AuroraPath.TabIndex = 5;
+            this.AuroraPath.Text = "C:\\\\Aurora";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point (146, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size (75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler (this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add (this.AuroraPath);
+            this.panel1.Controls.Add (this.label2);
+            this.panel1.Controls.Add (this.button1);
+            this.panel1.Controls.Add (this.button2);
+            this.panel1.Controls.Add (this.button3);
+            this.panel1.Controls.Add (this.ConfigFilePath);
+            this.panel1.Controls.Add (this.label1);
+            this.panel1.Location = new System.Drawing.Point (12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size (310, 92);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add (this.label3);
+            this.panel2.Controls.Add (this.initoGenerateName);
+            this.panel2.Controls.Add (this.button4);
+            this.panel2.Location = new System.Drawing.Point (12, 110);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size (310, 140);
+            this.panel2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point (227, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size (75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Generate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler (this.button4_Click);
+            // 
+            // initoGenerateName
+            // 
+            this.initoGenerateName.Location = new System.Drawing.Point (6, 16);
+            this.initoGenerateName.Name = "initoGenerateName";
+            this.initoGenerateName.Size = new System.Drawing.Size (215, 20);
+            this.initoGenerateName.TabIndex = 1;
+            this.initoGenerateName.TextChanged += new System.EventHandler (this.initoGenerateName_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point (3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size (120, 95);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler (this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point (3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size (133, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Generate new instance file";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point (6, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size (75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Start";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler (this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add (this.button8);
+            this.groupBox1.Controls.Add (this.button6);
+            this.groupBox1.Controls.Add (this.button5);
+            this.groupBox1.Location = new System.Drawing.Point (296, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size (89, 108);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point (6, 69);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size (75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Remove";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler (this.button6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point (129, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size (110, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Set Regions Directory";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point (129, 43);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size (75, 23);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Update";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler (this.button7_Click);
+            // 
+            // RegionsDir
+            // 
+            this.RegionsDir.Location = new System.Drawing.Point (129, 20);
+            this.RegionsDir.Name = "RegionsDir";
+            this.RegionsDir.Size = new System.Drawing.Size (100, 20);
+            this.RegionsDir.TabIndex = 14;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point (6, 44);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size (75, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Start (32 bit)";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler (this.button8_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add (this.listBox1);
+            this.panel3.Controls.Add (this.groupBox1);
+            this.panel3.Controls.Add (this.RegionsDir);
+            this.panel3.Controls.Add (this.button7);
+            this.panel3.Controls.Add (this.label4);
+            this.panel3.Location = new System.Drawing.Point (328, 126);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size (388, 124);
+            this.panel3.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size (728, 262);
+            this.Controls.Add (this.panel3);
+            this.Controls.Add (this.panel2);
+            this.Controls.Add (this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler (this.Form1_Load);
+            this.panel1.ResumeLayout (false);
+            this.panel1.PerformLayout ();
+            this.panel2.ResumeLayout (false);
+            this.panel2.PerformLayout ();
+            this.groupBox1.ResumeLayout (false);
+            this.panel3.ResumeLayout (false);
+            this.panel3.PerformLayout ();
+            this.ResumeLayout (false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox ConfigFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AuroraPath;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox initoGenerateName;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox RegionsDir;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ InstanceManager/InstanceManager/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace InstanceManager
+{
+    public partial class Form1 : Form
+    {
+        private List<string> list = new List<string> ();
+        private string m_selectedInstance = "";
+        private static string DefaultInstanceText = @"[RegionStartup]
+Default = RegionLoaderFileSystem
+RegionsDirectory = Regions";
+
+        public Form1 ()
+        {
+            InitializeComponent ();
+        }
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+            LoadFromConfig ();
+            RegenerateList ();
+        }
+
+        private void LoadFromConfig ()
+        {
+            if (!File.Exists ("Settings.txt"))
+                return;
+            string[] lines = File.ReadAllLines ("Settings.txt");
+            foreach (string line in lines)
+            {
+                string[] split = line.Split ('=');
+                switch (split[0])
+                {
+                    case "AuroraPath":
+                        AuroraPath.Text = split[1];
+                        break;
+                    case "ConfigFilePath":
+                        ConfigFilePath.Text = split[1];
+                        break;
+                    case "Instances":
+                        list.AddRange (split[1].Split (','));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void SaveToConfig ()
+        {
+            List<string> lines = new List<string> ();
+            lines.Add ("AuroraPath=" + AuroraPath.Text);
+            lines.Add ("ConfigFilePath=" + ConfigFilePath.Text);
+            lines.Add ("Instances=" + string.Join(",", list.ToArray()));
+
+            File.WriteAllLines ("Settings.txt", lines.ToArray());
+        }
+
+        private void button2_Click (object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = ConfigFilePath.Text;
+            if (!Directory.Exists (ConfigFilePath.Text))
+                Directory.CreateDirectory (ConfigFilePath.Text);
+            DialogResult result = folderBrowserDialog1.ShowDialog ();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                ConfigFilePath.Text = folderBrowserDialog1.SelectedPath;
+                SaveToConfig ();
+            }
+        }
+
+        private void button3_Click (object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = AuroraPath.Text;
+            if (!Directory.Exists (AuroraPath.Text))
+                Directory.CreateDirectory (AuroraPath.Text);
+            DialogResult result = folderBrowserDialog1.ShowDialog ();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                AuroraPath.Text = folderBrowserDialog1.SelectedPath;
+                SaveToConfig ();
+            }
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            if (!Directory.Exists (AuroraPath.Text))
+            {
+                MessageBox.Show ("Wrong Aurora path");
+                return;
+            }
+
+            #region Migrate the configs
+
+            string SourcePath = AuroraPath.Text;
+            string DestinationPath = ConfigFilePath.Text;
+            
+            //Copy all the files
+            foreach (string newPath in Directory.GetFiles (SourcePath, "*.ini", SearchOption.AllDirectories))
+            {
+                string dir = Path.GetDirectoryName (newPath.Replace (SourcePath, DestinationPath));
+                if (!Directory.Exists (dir))
+                    Directory.CreateDirectory (dir);
+                File.Copy (newPath, newPath.Replace (SourcePath, DestinationPath), true);
+            }
+
+            #endregion
+
+            if (File.Exists (Path.Combine (ConfigFilePath.Text, "DefaultInstance.ini")))
+                File.Delete (Path.Combine (ConfigFilePath.Text, "DefaultInstance.ini"));
+            File.WriteAllText (Path.Combine (ConfigFilePath.Text, "DefaultInstance.ini"), DefaultInstanceText);
+        }
+
+        private void button4_Click (object sender, EventArgs e)
+        {
+            string fileName = initoGenerateName.Text;
+            if (fileName != "")
+            {
+                File.Copy (Path.Combine (ConfigFilePath.Text, "DefaultInstance.ini"), Path.Combine (ConfigFilePath.Text, fileName));
+                list.Add (fileName);
+                SaveToConfig ();
+                RegenerateList ();
+            }
+        }
+
+        private void RegenerateList ()
+        {
+            listBox1.Items.Clear ();
+            foreach (string i in list)
+            {
+                listBox1.Items.Add (i);
+            }
+        }
+
+        private void initoGenerateName_TextChanged (object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click (object sender, EventArgs e)
+        {
+            if (m_selectedInstance == null)
+            {
+                MessageBox.Show ("Select an instance first");
+                return;
+            }
+
+            //Update regions dir
+            if (!Directory.Exists (RegionsDir.Text))
+                Directory.CreateDirectory (RegionsDir.Text);
+
+            string[] lines = File.ReadAllLines (Path.Combine (ConfigFilePath.Text, m_selectedInstance));
+            List<string> newLines = new List<string> (lines.Length);
+            foreach (string line in lines)
+            {
+                string newLine = line;
+                if (line.Contains ("RegionsDirectory = "))
+                    newLine = "RegionsDirectory = \"" + RegionsDir.Text + "\"";
+                newLines.Add (newLine);
+            }
+            File.WriteAllLines (Path.Combine (ConfigFilePath.Text, m_selectedInstance), newLines.ToArray ());
+        }
+
+        private void listBox1_SelectedIndexChanged (object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                RegionsDir.Text = "";
+                m_selectedInstance = "";
+            }
+            else
+            {
+                m_selectedInstance = listBox1.SelectedItem.ToString ();
+                string[] lines = File.ReadAllLines (Path.Combine (ConfigFilePath.Text, m_selectedInstance));
+                List<string> newLines = new List<string> (lines.Length);
+                foreach (string line in lines)
+                {
+                    if (line.Contains ("RegionsDirectory = "))
+                        RegionsDir.Text = line.Replace ("RegionsDirectory = ", "");
+                }
+            }
+        }
+
+        private void button6_Click (object sender, EventArgs e)
+        {
+            if (m_selectedInstance != "")
+            {
+                list.Remove (m_selectedInstance);
+                File.Delete (Path.Combine (ConfigFilePath.Text, m_selectedInstance));
+                m_selectedInstance = null;
+                SaveToConfig ();
+                RegenerateList ();
+            }
+        }
+
+        private void button5_Click (object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new ProcessStartInfo (Path.Combine(AuroraPath.Text, "Aurora.exe"));
+            info.Arguments = "-mainIniDirectory=" + ConfigFilePath.Text + " -secondaryIniFileName=" + m_selectedInstance;
+            info.UseShellExecute = true;
+            Process.Start (info);
+        }
+
+        private void button8_Click (object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new ProcessStartInfo (Path.Combine (AuroraPath.Text, "Aurora32bitLaunch.exe"));
+            info.Arguments = "-mainIniDirectory=" + ConfigFilePath.Text + " -secondaryIniFileName=" + m_selectedInstance;
+            info.UseShellExecute = true;
+            Process.Start (info);
+        }
+    }
+}
